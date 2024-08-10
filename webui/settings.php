e133b500faaa95ca2874ed7a0647a40c24a92dee
Jdawg Mcgee <mopenstein@gmail.com>
2024-08-10 08:00:55 -0400

    WEBUI Settings editor
    Added some detailed information about various settings

--- webui/settings.php
+++ webui/settings.php
@@ -136,33 +136,86 @@ var myInput = document.getElementById("editor");
     }
 </script>
 
-<h3>name:</h3>
+<h1>Base settings:</h1>
+<h2><em>Details concerning base settings</em></h2>
+	<h3>version:</h3>
+	<ul>
+		<li><i>float&gt;</i> the version number to which the main script compares ensuring compatibility</li>
+	</ul>
+	<h3>name:</h3>
+	<ul>
+		<li><i>string&gt;</i> used to identify the script in the web front end</li>
+	</ul>
+	<h3>drive:</h3>
+	<ul>
+		<li><i>[ string&gt;, array&gt; ]</i> list of strings containing locations of programming content. <i>(will automatically be converted to %D[index]% keyword)</i></li>
+	</ul>
+	<h3>insert_commercials:</h3>
+	<ul>
+		<li><i>boolean&gt;</i> setting to true will attempt to insert commercials into programming, false will disable this practice</li>
+	</ul>
+	<h3>commercials_per_break:</h3>
+	<ul>
+		<li><i>string&gt;</i> OR <i>whole integer&gt;</i> can be set to <b>'auto'</b> to allow the script to insert commercials based on lengths of video or a <b>whole integer</b> to force a set number of commercials to be played per break</li>
+	</ul>
+	<h3>time_test:</h3>
+	<ul>
+		<li><i>string&gt;</i> python date and time in string format which will force the script to program based on that time. (example: "Jul 18 2023 11:59AM")</li>
+	</ul>
+	<h3>report_data:</h3>
+	<ul>
+		<li><i>boolean&gt;</i> if set to false, no out going internet requests will be permitted (not even local requests).</li>
+	</ul>
+	<h3>player_settings:</h3>
+	<ul>
+		<li><i>[ string&gt;, array&gt; ]</i> list of arguments/commands to be sent to OMXplayer upon playing a video</li>
+	</ul>
+<h1>times/commercial_times:</h1>
+<h2><em>Details concerning programming</em></h2>
 <ul>
-	<li>An array of paths to directorys that contain either video files or sub-directories that contain video files.</li>
-	<li>special words: %D[NUM]% - sets the "drive" (ie base path) based on the the locations where <em>NUM</em> is the index corresponding to what was set in the "<a href="#drive">drive</a>" setting.</li>
+	<h3>name:</h3>
+	<ul>
+		<li>An array of paths to directorys that contain either video files or sub-directories that contain video files.</li>
+		<li>special words: %D[NUM]% - sets the "drive" (ie base path) based on the the locations where <em>NUM</em> is the index corresponding to what was set in the "<a href="#drive">drive</a>" setting.</li>
+	</ul>
+	<h3>dayOfWeek:</h3>
+	<ul>
+		<li>An array of days of the week on which the content can be played.</li>
+		<li>special words: monday, tuesday, wednesday,thursday,friday,saturday,sunday and the wild cards all,any,*.</li>
+		<li>setting as <em>null</em> is the same as using the wild card special words</li>
+	</ul>
+	<h3>month:</h3>
+	<ul>
+		<li>An array of numbers that correspond to the months of the year on which the content can be played. example: 1 - for the month of january</li>
+		<li>special words: wild cards all,any,*.</li>
+		<li>setting as <em>null</em> is the same as using the wild card special words</li>
+	</ul>
+	<h3>type:</h3>
+	<ul>
+		<li><i>&lt;string&gt;</i> - type of video to be played (defines how the script will organize, search for, and playback video)</li>
+		<br />
+		<ul>
+			<li>video = selects random video from a directory consisting only of video files ignoring everything else</li>
+			<li>video-show = selects random "show" from a directory consisting only of video files trying to balance the "shows" based on total play count</li>
+			<li>balanced-video = selects a random video from a single directory balanced around total video play count. If more than a single directory is supplied, one will be chosen based on a weighted play count.</li>
+			<li>ordered-show = selects random "show" from from a directory filled with sub-directories containing video files for that "show" and also selects the next video file in ascending order based on what was previously selected</li>
+			<li>ordered-video = selects random video from from a directory filled with video files and also selects the next video file in ascending order based on what was previously selected from this directory</li>
+			<li>commercial = selects random video from a directory consisting only of video files ignoring everything else identifying itself as a "commercial"</li>
+		</ol>
+	</ul>
+	<h3>start:</h3>
+	<ul>
+		<li><i>[&lt;integer&gt;Hour, &lt;integer&gt;Minute]</i> defines the Hour and Minute for which the programmed video will be played</li>
+	</ul>
+	<h3>end:</h3>
+	<ul>
+		<li><i>[&lt;integer&gt;Hour, &lt;integer&gt;Minute]</i> defines the Hour and Minute for which the programmed video will cease being played</li>
+	</ul>
+	<h3>channel</h3>
+	<ul>
+		<li><i>&lt;string&gt;</i> when set video from only this 'channel' will be played</li>
+	</ul>
 </ul>
-<h3>dayOfWeek:</h3>
-<ul>
-	<li>An array of days of the week on which the content can be played.</li>
-	<li>special words: monday, tuesday, wednesday,thursday,friday,saturday,sunday and the wild cards all,any,*.</li>
-	<li>setting as <em>null</em> is the same as using the wild card special words</li>
-</ul>
-<h3>month:</h3>
-<ul>
-	<li>An array of numbers that correspond to the months of the year on which the content can be played. example: 1 - for the month of january</li>
-	<li>special words: wild cards all,any,*.</li>
-	<li>setting as <em>null</em> is the same as using the wild card special words</li>
-</ul>
-<h3>type:</h3>
-<ul>
-	<li>video = selects random video from a directory consisting only of video files ignoring everything else</li>
-	<li>video-show = selects random "show" from a directory consisting only of video files trying to balance the "shows" based on total play count</li>
-	<li>balanced-video = selects a random video from a single directory balanced around total video play count. If more than a single directory is supplied, one will be chosen based on a weighted play count.</li>
-	<li>ordered-show = selects random "show" from from a directory filled with sub-directories containing video files for that "show" and also selects the next video file in ascending order based on what was previously selected</li>
-	<li>ordered-video = selects random video from from a directory filled with video files and also selects the next video file in ascending order based on what was previously selected from this directory</li>
-	<li>commercial = selects random video from a directory consisting only of video files ignoring everything else identifying itself as a "commercial"</li>
-</ul>
-
 <em>Whole integers must be treated as numbers and floats must be treated as strings. This is a json requirement.</em>
 </body>
 </html>
